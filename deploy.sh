2b2f01849c1e20529b044df986e05bdb91c982b1
Andy Li <andy@onthewings.net>
2016-08-20 05:01:16 +0800

    [deploy] make things into variables such that forks can override them easily
    The values can be inputted at TravisCI repo setting.

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ set -e # exit with nonzero exit code if anything fails
 if ! [ "$TRAVIS_HAXE_VERSION" == "3.2.1" ]; then
     echo "Skipping pushing to GitHub pages - that is only done for Haxe 3.2.1.";
     exit 0;
-elif [ -z "$GH_TOKEN" ]; then
-    echo "Skipping pushing to GitHub pages - missing $GH_TOKEN (probably in a PR build).";
+elif [ -z "$GHP_BOT_TOKEN" ]; then
+    echo "Skipping pushing to GitHub pages - missing GHP_BOT_TOKEN (probably in a PR build).";
     exit 0;
 else
     echo "Pushing GitHub pages"
@@ -16,11 +16,11 @@ cd output
 git init
 
 # inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "mark.knol@gmail.com"
+git config user.name "${GHP_BOT_NAME}"
+git config user.email "${GHP_BOT_EMAIL}"
 
 # add the CNAME file
-echo haxdevelop.org > CNAME
+echo "${GHP_CNAME}" > CNAME
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
@@ -31,4 +31,4 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@github.com/HaxeFoundation/haxedevelop.org.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GHP_BOT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
